db2b5ab762d0e0bbe0d187451be8534b758b2e88
Emanuel Fontelles <emanuel@datarisk.io>
2022-04-24 00:01:28 -0300

    update readme

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-async function fetchData() {
-    const res = await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
-    const record = await res.json();
-    console.log(record);
-    document.getElementById("date").innerHTML=record.data[0].date;
-    document.getElementById("areaName").innerHTML=record.data[0].areaName;
-    document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
-    document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
-}
-fetchData();
+const button = document.querySelector('search');
+
+// add click event listener
+button.addEventListener('click', () => {
+
+    // open a new tab
+    const tab = window.open('https://attacomsian.com', '_blank');
+
+});
+
